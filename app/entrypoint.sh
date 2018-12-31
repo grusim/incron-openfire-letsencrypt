2f1f21d40d4ca2bdc8e2c3272e6171dedc9f0305
flux <mail@cyb3rwr3ck.net>
2018-12-31 12:07:17 +0100

    - Added side-by-side root and non root config - Import of multiple scripts and watchfiles during build - Small corrections - README

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
-/usr/sbin/adduser -S -D -H -g "" ${INCRONUSER}
-echo ${INCRONUSER} > /etc/incron.allow
+/usr/sbin/addgroup ${INCRONUSER}
+/usr/sbin/adduser -s /bin/sh -D -H -h /dev/null -g "" -G ${INCRONUSER} ${INCRONUSER}
+echo root > /etc/incron.allow
+echo ${INCRONUSER} >> /etc/incron.allow
 
 #import watch files to non root incron user tab
-incrontab -u ${INCRONUSER} /usr/share/incron/${OPENFIRE_CERT_SCRIPT}
+for f in ${WATCHFILEDIR}/*;
+  do incrontab -u ${INCRONUSER} $f
+  echo "Imported" $f
+done
 
 exec "$@"
